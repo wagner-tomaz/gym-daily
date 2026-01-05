1ff08ddf204385878007fb9d07784021dd2a3f59
Wagner Tomaz <wagnerwillian.junior@gmail.com>
2026-01-05 03:52:02 +0000

    mobile app update

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'gym-daily-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './images/gymdaily.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Instalação: Cacheia os arquivos essenciais
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch: Tenta pegar do cache primeiro (funciona offline), se não tiver, vai na rede
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
